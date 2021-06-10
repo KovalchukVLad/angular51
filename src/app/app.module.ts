5ed1e6af0b0f9bfaf4743c49e8ab9d820754dad5
KovalchukVlad <vla.ik257@gmail.com>
2021-06-10 16:55:08 +0200

    angular51(2ed)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent,
+    UsersComponent,
+    TemplateFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
